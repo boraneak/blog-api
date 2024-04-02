36eb611c563e5ab1bcbd6734a5646879184e40fa
boraneak <boraneak123@gmail.com>
2024-04-03 04:59:48 +0700

    upvote & downvote article

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.send('hello, ur sever is up and running...!');
 })
@@ -16,6 +18,48 @@ app.get('/hello/:name', (req, res) => {
   console.log(name);
   res.end();
 });
+
+// fake articles' data
+const articleInfo = [
+  {
+    name: 'learn-react',
+    upvotes: 0,
+    downvotes: 0
+  },
+  {
+    name: 'learn-node',
+    upvotes: 0,
+    downvotes: 0
+  },
+  {
+    name: 'mongodb',
+    upvotes: 0,
+    downvotes: 0
+  },
+];
+// upvote
+app.put('/api/articles/:name/upvote', (req, res) => {
+  const { name } = req.params;
+  const article = articleInfo.find(a => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`the ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send('aricle not found');
+  }
+});
+//downvote
+app.put('/api/articles/:name/downvote', (req, res) => {
+  const { name } = req.params;
+  const article = articleInfo.find(a => a.name === name);
+  if (article) {
+    article.upvotes -= 1;
+    res.send(`the ${name} article now has ${article.upvotes} downvotes`);
+  } else {
+    res.send('aricle not found');
+  }
+});
+
 const port = 3001;
 
 app.listen(port, () => {
