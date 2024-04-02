33a9aff566d9792517e292aeba8b59329850f90d
boraneak <boraneak123@gmail.com>
2024-04-02 22:36:38 +0700

    how to send json data in express

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('hello, ur sever is up and running...!');
 })
 
+app.post('/', (req, res) => {
+  console.log(`hello ${req.body.name}`);
+  res.end();
+});
 const port = 3001;
 
 app.listen(port, () => {
